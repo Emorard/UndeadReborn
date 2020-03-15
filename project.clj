49cdea25cce1cf9db88cce69bacb6c9c5d89a265
Emorard <emorard@outlook.jp>
2020-03-15 14:23:47 +0900

    2.7-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,10 +1,13 @@
-(defproject UndeadReborn "2.3-SNAPSHOT"
+(defproject UndeadReborn "2.7-SNAPSHOT"
      :java-source-paths ["java-src"]
      :javac-options ["-d" "classes/" "-source" "1.8" "-target" "1.8"]
-     :repositories [ ["spigot-repo" "https://hub.spigotmc.org/nexus/content/repositories/snapshots/"] ]
+     :repositories [ ["spigot-repo" "https://hub.spigotmc.org/nexus/content/repositories/snapshots/"]
+                     ["sk89q-repo" "https://maven.enginehub.org/repo/"]]
      :dependencies [ [org.clojure/clojure "1.10.1"] ]
      :profiles {
-                :provided {:dependencies [[org.spigotmc/spigot-api "1.12-R0.1-SNAPSHOT"]]}
+                :provided {:dependencies [[org.spigotmc/spigot-api "1.12-R0.1-SNAPSHOT"]
+                                          [com.sk89q.worldguard/worldguard-bukkit "7.0.0"]
+                                          [com.sk89q.worldedit/worldedit-bukkit "7.0.0"]]}
                 :uberjar {:uberjar-name "UndeadReborn.jar" :aot :all}
                 }
      )
--- src/undeadreborn/URListener.clj
+++ src/undeadreborn/URListener.clj
@@ -16,10 +16,15 @@
            (org.bukkit.inventory ItemStack)
            (org.bukkit.inventory.meta SkullMeta)
            (org.bukkit.event.server PluginEnableEvent)
-           (org.bukkit.attribute Attribute)))
+           (org.bukkit.attribute Attribute)
+           (org.bukkit.potion PotionEffect PotionEffectType)
+           (com.sk89q.worldguard.bukkit WorldGuardPlugin)))
 
 (def entityType {"ZOMBIE" EntityType/ZOMBIE, "PLAYER" EntityType/PLAYER})
 (def attribute {"GENERIC_MOVEMENT_SPEED" Attribute/GENERIC_MOVEMENT_SPEED, "GENERIC_MAX_HEALTH" Attribute/GENERIC_MAX_HEALTH})
+
+(def worldguard (WorldGuardPlugin/inst))
+
 ; ゾンビのインベントリマップ
 (def inventory-map (HashMap.))
 ; Undroppableなアイテムマップ
@@ -27,11 +32,19 @@
 
 (defn getField [this key] (@(.state this) key))
 
+(defn parse-int [#^String i] (try (Integer/parseInt (re-find #"\d+" i)) (catch NumberFormatException e 100)))
+
+(defn undroppable? [#^String lore] (if (nil? lore) false (>= (parse-int lore) (+ 1 (rand-int 99)))))
+
+(defn isPokkeTown [#^Player p]
+  (let [region (.getRegion (.getRegionManager worldguard (.getWorld p)) "pokketown") l (.getLocation p)]
+    (.contains region (.getX l) (.getY l) (.getZ l))))
+
 (defn -init [^Main plugin]
   [[] (atom {:plugin plugin :world (.getString (.getConfig plugin) "world")})])
 
 (defn playerSkull [^Player player]
-  (let [item (ItemStack. Material/SKULL_ITEM 1 (short 3))]
+  (let [item (ItemStack. (Material/getMaterial "SKULL_ITEM") 1 (short 3))]
     (let [sm ^SkullMeta(.getItemMeta item)]
       (.setOwner sm (.getName player))
       (.setItemMeta item sm)
@@ -48,31 +61,37 @@
               (.dropItem world location drop)
               (.remove inventory-map uuid)))))
       (= (.getEntityType event) (entityType "PLAYER"))
-      (let [player ^Player(.getEntity event)]
-        (when (and (= [(.getName (.getWorld player)) (getField this :world)]) (= [(.getGameMode player) (GameMode/SURVIVAL)]))
-          (let [zombie ^Zombie(.spawnEntity (.getWorld (.getEntity event)) (.getLocation (.getEntity event)) (entityType "ZOMBIE"))]
-            (-> zombie (.getAttribute (attribute "GENERIC_MOVEMENT_SPEED")) (.setBaseValue 0.35))
-            (-> zombie (.getAttribute (attribute "GENERIC_MAX_HEALTH")) (.setBaseValue 5.0))
-            (.setHealth zombie 5.0)
-            (.setCustomName zombie (str (.getName player) "'s Zombie"))
-            (.setCustomNameVisible zombie true)
-            (.setRemoveWhenFarAway zombie false)
-            ; プレイヤーゾンビを作成するための処理
-            (let [cloned (ArrayList. (filter #(instance? ItemStack %) (.getDrops event))) equip (.getEquipment (cast LivingEntity zombie)) pEquip (.getEquipment player)]
-              (-> equip (.setItemInMainHand (.getItemInMainHand pEquip))) (.setItemInMainHandDropChance equip 0)
-              (-> equip (.setItemInOffHand (.getItemInOffHand pEquip))) (.setItemInOffHandDropChance equip 0)
-              (-> equip (.setHelmet (playerSkull player))) (.setHelmetDropChance equip 0)
-              (-> equip (.setChestplate (.getChestplate pEquip))) (.setChestplateDropChance equip 0)
-              (-> equip (.setLeggings (.getLeggings pEquip))) (.setLeggingsDropChance equip 0)
-              (-> equip (.setBoots (.getBoots pEquip))) (.setBootsDropChance equip 0)
-              ; Undroppableなアイテム
-              (def undroppable (filter #(and (.hasItemMeta %) (.hasLore (.getItemMeta %)) (.contains (.getLore (.getItemMeta %)) (ChatColor/translateAlternateColorCodes (char (int \&)) "&6Undroppable"))) cloned))
-              (when (not-empty undroppable)
-                (.removeAll cloned undroppable)
-                (.put undroppable-map (.getUniqueId player) (ArrayList. undroppable)))
-              (.put inventory-map (.getUniqueId zombie) cloned)
-              ; ClojureはSeqは遅延するのに注意
-              (.clear (.getDrops event)))))))))
+      (let [player ^Player (.getEntity event)]
+        (if (isPokkeTown player)
+          (let [contents (ArrayList. (filter #(instance? ItemStack %) (.getDrops event)))]
+            (when (not-empty contents) (.put undroppable-map (.getUniqueId player) contents))
+            (.clear (.getDrops event)))
+          (when (and (= (.getName (.getWorld player)) (getField this :world)) (= (.getGameMode player) (GameMode/SURVIVAL)))
+            (let [zombie ^Zombie (.spawnEntity (.getWorld (.getEntity event)) (.getLocation (.getEntity event)) (entityType "ZOMBIE"))]
+              (-> zombie (.getAttribute (attribute "GENERIC_MOVEMENT_SPEED")) (.setBaseValue 0.35))
+              (-> zombie (.getAttribute (attribute "GENERIC_MAX_HEALTH")) (.setBaseValue 5.0))
+              (-> zombie (.addPotionEffect (PotionEffect. (PotionEffectType/SLOW) 60 10)))
+              (-> zombie (.addPotionEffect (PotionEffect. (PotionEffectType/WEAKNESS) 60 10)))
+              (.setHealth zombie 5.0)
+              (.setCustomName zombie (str (.getName player) "'s Zombie"))
+              (.setCustomNameVisible zombie true)
+              (.setRemoveWhenFarAway zombie false)
+              ; プレイヤーゾンビを作成するための処理
+              (let [cloned (ArrayList. (filter #(instance? ItemStack %) (.getDrops event))) equip (.getEquipment (cast LivingEntity zombie)) pEquip (.getEquipment player)]
+                (-> equip (.setItemInMainHand (.getItemInMainHand pEquip))) (.setItemInMainHandDropChance equip 0)
+                (-> equip (.setItemInOffHand (.getItemInOffHand pEquip))) (.setItemInOffHandDropChance equip 0)
+                (-> equip (.setHelmet (playerSkull player))) (.setHelmetDropChance equip 0)
+                (-> equip (.setChestplate (.getChestplate pEquip))) (.setChestplateDropChance equip 0)
+                (-> equip (.setLeggings (.getLeggings pEquip))) (.setLeggingsDropChance equip 0)
+                (-> equip (.setBoots (.getBoots pEquip))) (.setBootsDropChance equip 0)
+                ; Undroppableなアイテム
+                (def undroppable (filter #(and (.hasItemMeta %) (.hasLore (.getItemMeta %)) (undroppable? (re-find #"Undroppable.*\%" (clojure.string/join "," (.getLore (.getItemMeta %)))))) cloned))
+                (when (not-empty undroppable)
+                  (.removeAll cloned undroppable)
+                  (.put undroppable-map (.getUniqueId player) (ArrayList. undroppable)))
+                (when (not-empty cloned) (.put inventory-map (.getUniqueId zombie) cloned))
+                ; ClojureはSeqは遅延するのに注意
+                (.clear (.getDrops event))))))))))
 
 (defn -onRespawn [this #^PlayerRespawnEvent event]
   (let [uuid (.getUniqueId (.getPlayer event))]
@@ -93,4 +112,6 @@
   (when (= (.getName (.getPlugin event)) "UndeadReborn")
     (println "Cleanup zombies...")
     (cleanUp this)))
+
+
 
